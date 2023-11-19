54389b31cfd93a55caadc39192968b008d87e690
Vincent Pastor <vincent.pastor.pro@gmail.com>
2023-11-19 09:33:56 +0100

    Corrected focus on modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,8 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    const firstName = document.getElementById('firstName');
+    firstName.focus();
 }
 
 function closeModal() {
